fb3136110d2408c917a271a787049499e3fa0529
Jia Jing <lls@administratordemacbook-pro-52.local>
2018-07-31 12:48:49 +0800

    Initial Commit

---
+++ aubioDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aubioDemo
+//
+//  Created by lls on 2018/7/31.
+//  Copyright © 2018年 demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
